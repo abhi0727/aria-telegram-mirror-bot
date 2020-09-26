e252d57c8b3231d37815c65bcfe2ed0a1ab7401b
“abhijit” <lordhacker348@gmail.com>
2020-09-26 12:18:35 +0530

    botify

---
+++ out/.constants.js
@@ -0,0 +1,28 @@
+module.exports = Object.freeze({
+    TOKEN: '1320029010:AAEAFQzDtbmU4-DVXSnXONMjd9NCY0XIqSs',
+    ARIA_SECRET: 'abhijit',
+    ARIA_DOWNLOAD_LOCATION: '/home/helbertderek/mirror-bot',
+    ARIA_DOWNLOAD_LOCATION_ROOT: '/',
+    ARIA_FILTERED_DOMAINS: ['yts', 'YTS', 'cruzing.xyz', 'eztv.ag', 'YIFY'],
+    ARIA_FILTERED_FILENAMES: ['YIFY'],
+    ARIA_PORT: 8210,
+    GDRIVE_PARENT_DIR_ID: '1-4GambDFJ_fyfq7x8-LOxOfZX_VHVR4R',
+    SUDO_USERS: [628449197, 345],
+    AUTHORIZED_CHATS: [678, 901],
+    STATUS_UPDATE_INTERVAL_MS: 12000,
+    DRIVE_FILE_PRIVATE: {
+        ENABLED: false,
+        EMAILS: ['someMail@gmail.com', 'someOtherMail@gmail.com']
+    },
+    DOWNLOAD_NOTIFY_TARGET: {
+        enabled: true,
+        host: 'hostname.domain',
+        port: 80,
+        path: '/botNotify'
+    },
+    COMMANDS_USE_BOT_NAME: {
+        ENABLED: false,
+        NAME: "@lord_mirrorbot"
+    },
+    IS_TEAM_DRIVE: true
+});
